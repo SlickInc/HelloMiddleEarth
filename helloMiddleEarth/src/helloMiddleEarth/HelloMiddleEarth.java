21c3e598677912336a3cb9e2091914143bd36ac1
Ivan <ivennsfroese@gmail.com>
2021-10-19 13:42:11 -0500

    Adding A Greeting

---
+++ helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
@@ -0,0 +1,14 @@
+package helloMiddleEarth;
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+		
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		//print greetings
+		for (int i = 0; i <names.length; i++) {
+			System.out.println("Hello, " + names[i]);
+		}
+	}
+
+}
