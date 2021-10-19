2069411b65f646564755b04bcde0355be053c1aa
Ivan2 <ivennsfroese2@gmail.com>
2021-10-19 14:03:02 -0500

    added a print out and two new characters

--- helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
+++ helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
@@ -4,11 +4,12 @@ public class HelloMiddleEarth {
 
 	public static void main(String[] args) {
 		
-		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Boromir", "Faramir"};
 		//print greetings
 		//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
 		for (int i = 0; i <names.length; i++) {
 			System.out.println("Hello, " + names[i]);
+			System.out.println("How are you doing today?");
 		}
 	}
 
