cb024e55923aa94e67d18f73b98e43ec9ea80155
Ivan <ivennsfroese@gmail.com>
2021-10-19 13:56:59 -0500

    Converted for loop

--- helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
+++ helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
@@ -5,10 +5,10 @@ public class HelloMiddleEarth {
 	public static void main(String[] args) {
 		
 		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
-		//print greetings
+		//print greetings to the fine folks in the Middle Earth
 		//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
-		for (int i = 0; i <names.length; i++) {
-			System.out.println("Hello, " + names[i]);
+		for (String name : names) {
+			System.out.println("Hello, " + name);
 		}
 	}
 
