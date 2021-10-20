24181aa6bcffb026d99d87e9b5cef4a3bb2d49f0
Ivan <ivennsfroese@gmail.com>
2021-10-20 13:44:42 -0500

    Added loop

--- helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
+++ helloMiddleEarth/src/helloMiddleEarth/HelloMiddleEarth.java
@@ -1,18 +1,21 @@
 package helloMiddleEarth;
-
+import java.util.Scanner;
 public class HelloMiddleEarth {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
 		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Boromir", "Faramir"};
-		//print greetings to the fine folks in the Middle Earth
-		for (String name : names) {
-			System.out.println("Hello, " + name);
-			//something nothing
-			System.out.println("How are you doing today?");
-			
-		}
-		
+		do { 
+			//print greetings to the fine folks in the Middle Earth
+			for (String name : names) {
+				System.out.println("Hello, " + name);
+				//something nothing
+				System.out.println("How are you doing today?");
+				
+			}
+			System.out.println("Repeat?");
+		}while(input.next() == "y");
 	}
 
 }
